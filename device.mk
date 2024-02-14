13e0fc84da8c3df220271d33ab65700aa304879f
Cyber Knight <cyberknight755@gmail.com>
2024-02-14 20:41:08 +0000

    cupid: drop simplified xiaomi udfps hal and build full sensors hal

--- device.mk
+++ device.mk
@@ -451,8 +451,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge \
-    sensors.xiaomi \
-    sensors.udfps \
+    sensors.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
